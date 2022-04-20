0c26b927cd0657189287a528f1f805075f983434
ovillegas <orlando.villegas@univ-pau.fr>
2022-04-20 13:08:11 +0200

    adding module central to work with molecules

---
+++ moltools/ffield.py
@@ -0,0 +1,327 @@
+
+import numpy as np
+import networkx as nx
+import os
+import re
+from moltools.structure import ATOM
+
+
+class ForceFieldError(Exception):
+    pass
+
+
+# types of files sopported
+forcefields = ["gromos", "gaff", "oplsaa"]
+
+_errorMessages = {
+    0: """
+    \033[1;31mForce field is not found,
+    Force field supported to date: {}
+    \033[m""".format(", ".join(forcefields)),
+    1: """
+    \033[1;31mThe force field for {} - {} is not defined
+    Bonded with {}.
+    \033[m""",
+    2: """
+    \033[1;31mIt was not possible to assign atom type to {}
+    \033[m""",
+    3: """
+    \033[1;31mIt was not possible to assign atom type to :\n{}
+    \033[m"""
+}
+
+
+# database force field path
+_ffpath = os.path.dirname(os.path.realpath(__file__))
+
+
+def ffdata(ff):
+    """search the path of force field"""
+    return os.path.join(_ffpath, "forcefields", "%s.dat" % ff)
+
+
+def database(ff):
+    # VDW parameters
+    vdw = re.compile(
+        r"""
+        ^(?P<type>\w+)\s+
+        (?P<sigma>[+-]?\d+\.\d+e?[+-]?\d?\d?)\s+      # nm
+        (?P<epsilon>[+-]?\d+\.\d+e?[+-]?\d?\d?)\s+   # kJ/mol
+        """, re.X)
+    vdwpar = {}
+
+    # BONDS parameters
+    bonds = re.compile(
+        r"""
+        ^(?P<b1>\w+)\s*-\s*(?P<b2>\w+)\s+
+        (?P<kb>[+-]?\d+\.\d+)\s+           # kb kcal/mol/ang2
+        (?P<b0>[+-]?\d+\.\d+)\s+           # b0 ang
+        """, re.X)
+    bondspar = {}
+
+    # ANGLES parameters
+    angles = re.compile(
+        r"""
+        ^(?P<a1>\w+)\s*-\s*(?P<a2>\w+)\s*-\s*(?P<a3>\w+)\s+
+        (?P<kth>[+-]?\d+\.\d+)\s+          # kth kcal/mol/rad2
+        (?P<th0>[+-]?\d+\.\d+)\s+          # degree
+        """, re.X)
+    anglespar = {}
+
+    # DIHEDRALS parameters
+    dihedrals = re.compile(
+        r"""
+        ^(?P<d1>\w+)\s*-\s*(?P<d2>\w+)\s*-\s*(?P<d3>\w+)\s*-\s*(?P<d4>\w+)\s+
+        (?P<divider>[+-]?\d+)\s+               # int
+        (?P<Vn>[+-]?\d+\.\d+)\s+               # kcal/mol
+        (?P<phi>[+-]?\d+\.\d+)\s+              # degree
+        (?P<n>[+-]?\d\.\d*)\s+                # periodicity
+        """, re.X)
+    dihedralspar = {}
+    # print(ffdata(ff))
+    with open(ffdata(ff), "r") as DBASE:
+        for line in DBASE:
+            if vdw.match(line):
+                # VDW
+                m = vdw.match(line)
+                m = m.groupdict()
+                # print(m)
+                # exit()
+                vdwpar[m["type"]] = [
+                        np.float64(m["sigma"]), np.float64(m["epsilon"])
+                    ]
+            elif bonds.match(line):
+                # BONDS
+                m = bonds.match(line)
+                m = m.groupdict()
+                bond = (m["b1"], m["b2"])
+                # print(m)
+                # print(bond)
+                # exit()
+                bondspar[bond] = [
+                        np.float64(m["b0"]), np.float64(m["kb"])
+                    ]
+            elif angles.match(line):
+                # ANGLES
+                m = angles.match(line)
+                m = m.groupdict()
+                angle = (m["a1"], m["a2"], m["a3"])
+                # print(m)
+                # print(angle)
+                # exit()
+                anglespar[angle] = [
+                        np.float64(m["th0"]), np.float64(m["kth"])
+                    ]
+                anglespar[angle[::-1]] = [
+                        np.float64(m["th0"]), np.float64(m["kth"])
+                    ]
+            elif dihedrals.match(line):
+                # DIHEDRALS
+                m = dihedrals.match(line)
+                m = m.groupdict()
+                dih = (m["d1"], m["d2"], m["d3"], m["d4"])
+                # print(m)
+                # print("HOLA" * 80)
+                # print(dih)
+                # print(dih)
+                # exit()
+                dihedralspar[dih] = [
+                            np.int64(m["divider"]),
+                            np.float64(m["Vn"]),
+                            np.float64(m["phi"]),
+                            int(np.float64(m["n"]))
+                        ]
+                dihedralspar[dih[::-1]] = [
+                        np.int64(m["divider"]),
+                        np.float64(m["Vn"]),
+                        np.float64(m["phi"]),
+                        int(np.float64(m["n"]))
+                    ]
+    return {"vdw": vdwpar, "bonds": bondspar, "angles": anglespar, "dihedrals": dihedralspar}
+
+
+def get_interactions_list(MOL):
+    """Lists of interactions are generated."""
+
+    connect = MOL.connect
+    all_ps = dict(nx.algorithms.all_pairs_shortest_path_length(connect))
+    all_paths = []
+
+    for s in all_ps.keys():
+        for e in all_ps[s].keys():
+            if all_ps[s][e] == 1:
+                all_paths += list(nx.algorithms.all_simple_paths(connect, s, e, cutoff=1))
+            elif all_ps[s][e] == 2:
+                all_paths += list(nx.algorithms.all_simple_paths(connect, s, e, cutoff=2))
+            elif all_ps[s][e] == 3:
+                all_paths += list(nx.algorithms.all_simple_paths(connect, s, e, cutoff=3))
+    # BONDS, list, types
+    bonds_list = [tuple(p) for p in all_paths if len(set(p)) == 2]
+    for iat, jat in bonds_list:
+        if iat < jat:
+            MOL.bonds_list.append((iat, jat))
+    MOL.dfbonds["list"] = MOL.bonds_list
+
+    MOL.dfbonds["types"] = MOL.dfbonds["list"].apply(
+        lambda x: (
+            MOL.dftypes.loc[x[0], "type"],
+            MOL.dftypes.loc[x[1], "type"])
+        )
+    MOL.dfbonds["types"] = MOL.dfbonds["types"].apply(
+        lambda x: tuple(sorted(x)))
+    # ANGLES, list, types
+    angles_list = [tuple(p) for p in all_paths if len(set(p)) == 3]
+    for iat, jat, kat in angles_list:
+        if iat < kat:
+            MOL.angles_list.append((iat, jat, kat))
+    MOL.dfangles["list"] = MOL.angles_list
+
+    MOL.dfangles["types"] = MOL.dfangles["list"].apply(
+        lambda x: (
+            MOL.dftypes.loc[x[0], "type"],
+            MOL.dftypes.loc[x[1], "type"],
+            MOL.dftypes.loc[x[2], "type"])
+        )
+    # DIHEDRALS, list, types
+    dihedrals_list = [tuple(p) for p in all_paths if len(set(p)) == 4]
+    for iat, jat, kat, lat in dihedrals_list:
+        if iat < lat:
+            MOL.dihedrals_list.append((iat, jat, kat, lat))
+    MOL.dfdih["list"] = MOL.dihedrals_list
+
+    MOL.dfdih["types"] = MOL.dfdih["list"].apply(
+        lambda x: (
+            MOL.dftypes.loc[x[0], "type"],
+            MOL.dftypes.loc[x[1], "type"],
+            MOL.dftypes.loc[x[2], "type"],
+            MOL.dftypes.loc[x[3], "type"])
+        )
+
+    # print("Natoms:", connect.number_of_nodes())
+    # print("Bonds:\n", MOL.bonds_list)
+    # print("Nbonds:", len(MOL.bonds_list))
+    # print("Angles:\n", MOL.angles_list)
+    # print("Nangles:", len(MOL.angles_list))
+    # print("Dihedrals:\n", MOL.dihedrals_list)
+    # print("NDihedrals:", len(MOL.dihedrals_list))
+
+
+def assinging_at_type(atom):
+    error = _errorMessages[1].format(atom.n, atom.atsb, atom.atoms_connect)
+    if atom.atsb == "C":
+        """ CARBON """
+        if atom.atoms_connect.count("H") == 4:
+            return "c3"
+
+        elif atom.hyb == "sp3" and atom.atoms_connect.count("H") == 3:
+            return "c3"
+
+        elif atom.hyb == "sp3" and atom.atoms_connect.count("H") == 2:
+            return "c3"
+
+        else:
+            raise ForceFieldError(error)
+
+    elif atom.atsb == "H":
+        """ HYDROGEN """
+        if atom.atoms_connect == "Csp3":
+            return "hc"
+
+    else:
+        raise ForceFieldError(error)
+
+
+def get_atoms_types(MOL, ff):
+    """Assigns the atom timpos for the indicated force field."""
+
+    if ff == "gromos":
+        print("\033[1;35mForce field: GROMOS\033[m")
+        # GROMOS.Get_ATypes(MOL, ffdata(ff))
+
+    elif ff == "gaff":
+        print("\033[1;35mForce field: GAFF\033[m")
+        # ffdata = os.path.join(ffpath, "forcefields", "%s.dat" % self.ff)
+        # GAFF.Get_ATypes(MOL, ffdata(ff))
+        # GAFF.get_bonds(MOL, ffdata)
+    elif ff == "oplsaa":
+        print("\033[1;35mForce field: OPLS AS\033[m")
+
+    else:
+        raise ForceFieldError(_errorMessages[0])
+
+    coord = MOL.dfatoms
+    coord["type"] = "No Found"
+
+    # Central Iterator
+    for i in coord.index:
+        if coord.atsb[i] == "C":
+            """ CARBON """
+            C = ATOM("C", i)
+            coord.loc[C.n, "type"] = assinging_at_type(C)
+
+        elif coord.atsb[i] == "H":
+            """ HYDROGEN """
+            H = ATOM("H", i)
+            coord.loc[H.n, "type"] = assinging_at_type(H)
+
+        else:
+            raise ForceFieldError(_errorMessages[2].format(coord.atsb[i]))
+
+    print(coord)
+    MOL.dftypes = coord
+
+
+def get_ffparameters(MOL, ff):
+    dftypes = MOL.dftypes
+    dfbonds = MOL.dfbonds
+    dfangles = MOL.dfangles
+
+    # VDW
+    dbase = database(ff)
+    vdwpar = dbase["vdw"]
+    try:
+        dftypes["sigma"] = dftypes["type"].apply(lambda x: vdwpar[x][0])
+        dftypes["epsilon"] = dftypes["type"].apply(lambda x: vdwpar[x][1])
+    except KeyError:
+        raise ForceFieldError(_errorMessages[3].format(dftypes))
+    print(dftypes)
+
+    # BONDS
+    bondspar = dbase["bonds"]
+    try:
+        dfbonds["b0"] = dfbonds["types"].apply(lambda x: bondspar[x][0])
+        dfbonds["kb"] = dfbonds["types"].apply(lambda x: bondspar[x][1])
+    except KeyError:
+        # Exist bonds not found
+        bonds = set(dfbonds.types.values)
+        not_found = []
+        for b in bonds:
+            if b not in bondspar:
+                not_found.append(b)
+        raise ForceFieldError("""\033[1;31m
+                It was not possible to assign a bond type:{}
+                \033[m""".format(not_found))
+
+    for i in dfbonds.index:
+        ai = dfbonds.loc[i, "list"][0]
+        aj = dfbonds.loc[i, "list"][1]
+        MOL.connect[ai][aj]["dist"] = dfbonds.loc[i, "b0"]
+        MOL.connect[aj][ai]["dist"] = dfbonds.loc[i, "b0"]
+    print(dfbonds)
+
+    # ANGLES
+    anglespar = dbase["angles"]
+    try:
+        dfangles["th0"] = dfangles["types"].apply(lambda x: anglespar[x][0])
+        dfangles["kth"] = dfangles["types"].apply(lambda x: anglespar[x][1])
+    except KeyError:
+        # Exist angles not found
+        angles = set(dfangles.types.values)
+        not_found = []
+        for a in angles:
+            if a not in bondspar:
+                not_found.append(a)
+        raise ForceFieldError("""\033[1;31m
+                It was not possible to assign a angle type:{}
+                \033[m""".format(not_found))
+    print(dfangles)
---
+++ moltools/structure.py
@@ -0,0 +1,318 @@
+
+import pandas as pd
+import numpy as np
+import networkx as nx
+from scipy.spatial.distance import cdist
+from scipy.constants import e
+
+
+class MoleculeDefintionError(Exception):
+    pass
+
+
+# types of files sopported
+typesfiles = ["xyz"]
+
+# Defining error messages
+_errorMessages = {
+    0: """
+    \033[1;31mMolecule format is not recognized,
+    files supported to date: {}
+    \033[m""".format(", ".join(typesfiles)),
+    1: """
+    \033[1;31mNo structures have been loaded yet.
+    \033[m""",
+    2: """
+    \033[1;31mATOM is not configured to work with the atom: {}
+    \033[m""",
+    3: """
+    \033[1;31mHybridation for {} - {} not found"
+    \033[m""",
+}
+
+
+Elements = {  # g/mol
+    'H': {'mass': 1.0080, 'num': 1},
+    'C': {'mass': 12.011, 'num': 6},
+}
+
+
+class connectivity(nx.DiGraph):
+    """Building a class connectivity from directed graphs."""
+    def __init__(self):
+        super().__init__()
+
+    def get_connectivity(self, coord):
+        """Build connectivity from coordinates using nodes like atoms."""
+
+        # Add nodes using atoms andsymbols and coordinates
+        for i in coord.index:
+            self.add_node(
+                i,
+                xyz=coord.loc[i, ['x', 'y', 'z']].values,
+                atsb=coord.loc[i, 'atsb']
+            )
+
+        # Add edges like bonds
+        # get pairs atoms bonded
+        pairs, m = self._neighboring_pairs(coord)
+        for i, j in pairs:
+            self.add_edge(i, j)
+            self.add_edge(j, i)
+
+            self.edges[i, j]['dist'] = m[i, j]
+            self.edges[j, i]['dist'] = m[i, j]
+
+    def _neighboring_pairs(self, coord):
+        """Return neighboring pairs"""
+
+        xyz = coord.loc[:, ['x', 'y', 'z']].values.astype(np.float64)
+        # atoms = xyz = coord.loc[:, ['atsb']].values
+        # compute distance
+        m = cdist(xyz, xyz, 'euclidean')
+        m = np.triu(m)
+
+        indexs = np.where((m > 0.) & (m <= 1.7))
+        # print(indexs)
+        # print(indexs[0])
+        # print(coord["atsb"][indexs[0]])
+        # print(coord["atsb"][indexs[1]])
+        # exit()
+
+        pairs = map(lambda in0, in1: (in0, in1), indexs[0], indexs[1])
+
+        return pairs, m
+
+    @property
+    def atoms_map(self):
+        """
+        Returns a dict with the indices as key and the values as the atom
+        indices to which it is bonded.
+        """
+        return {at: list(self.neighbors(at)) for at in self.nodes}
+
+    def reset_nodes(self):
+        """Reset le count of node from 0 to sizes nodes -1"""
+        mapping = {value: count for count, value in enumerate(self.nodes, start=0)}
+
+        # return nx.relabel_nodes(self, mapping, copy=True)
+        self = nx.relabel_nodes(self, mapping, copy=True)
+
+    def nbonds(self, inode):
+        """Return number of atoms in connected to iat."""
+
+        return int(self.degree[inode] / 2)
+
+    def at_connect(self, inode):
+        """Return the atoms symbols conectec to i node"""
+
+        return '-'.join([self.nodes[a]['atsb'] for a in list(self.neighbors(inode))])
+
+    def get_df(self):
+        """Return a coordinate dataframe from connectivity."""
+        indexs = list(self.nodes)
+        rows = list()
+
+        for i in self.nodes:
+            rows.append({
+                'atsb': self.nodes[i]['atsb'],
+                'x': self.nodes[i]['xyz'][0],
+                'y': self.nodes[i]['xyz'][1],
+                'z': self.nodes[i]['xyz'][2]
+            })
+
+        df = pd.DataFrame(rows, index=indexs)
+
+        return df
+
+
+class MOL:
+    """
+    Class used to represent a molecule.
+
+    Attributes
+    ----------
+    dfatoms : DataFrame
+        Table with all the information of the atoms of the molecule, symbol,
+        mass and x and z coordinates.
+
+    Methods
+    -------
+
+    """
+
+    # Atoms
+    dfatoms = pd.DataFrame()
+    dftypes = pd.DataFrame()
+
+    atoms_count = {"C": 0, "N": 0, "H": 0}
+
+    dfbonds = pd.DataFrame()
+    dfangles = pd.DataFrame()
+    dfdih = pd.DataFrame()
+
+    # Connectivity
+    connect = connectivity()
+
+    bonds_list = []
+    angles_list = []
+    dihedrals_list = []
+
+    def load_file(self, file):
+        """Extract information from file."""
+
+        if file.endswith("xyz"):
+            MOL.dfatoms = _load_xyz(file)
+
+        else:
+            raise MoleculeDefintionError(_errorMessages[0])
+
+        # RES name from file name
+        self.res = file.split('/')[-1].split('.')[0].upper()
+
+        MOL.atoms_count.update(dict(MOL.dfatoms["atsb"].value_counts()))
+
+    def search_connectivity(self):
+        """Search connectivity from a dfatoms and stores it in an object
+        variable connect."""
+        if len(MOL.dfatoms) > 0:
+            MOL.connect.get_connectivity(MOL.dfatoms)
+        else:
+            raise MoleculeDefintionError(_errorMessages[1])
+
+    @property
+    def get_info(self):
+        """Returns a header with molecule information."""
+        info = ''
+        info += '#----------------------\n'
+        info += '# Molecular info: {}\n'.format(self.res)
+        info += '#----------------------\n'
+        info += '# MM {:.3f} g/mol\n'.format(self.mm)
+        info += '# DBE {:d}\n'.format(self.DBE)
+        info += '# Formule {}\n'.format(self.formule)
+        info += '# Dipolar {:.3f} D\n'.format(self.mu)
+        info += '#----------------------\n'
+        return info
+
+    @property
+    def mu(self):
+        """Compute the moment dipolar."""
+        x = MOL.dfatoms.x.values
+        y = MOL.dfatoms.y.values
+        z = MOL.dfatoms.z.values
+        q = MOL.dfatoms.charge.values
+        mu_x = np.sum(q * x * (4.8 / 1.6e-29) * e) * 1e-10
+        mu_y = np.sum(q * y * (4.8 / 1.6e-29) * e) * 1e-10
+        mu_z = np.sum(q * z * (4.8 / 1.6e-29) * e) * 1e-10
+        return np.sqrt(mu_x**2 + mu_y**2 + mu_z**2)
+
+    @property
+    def DBE(self):
+        """Return the DBE (Double Bond Equivalent)."""
+        atoms = MOL.atoms_count
+        return int((2 * atoms["C"] + atoms["N"] + 2 - atoms["H"]) / 2)
+
+    @property
+    def mm(self):
+        """Return the Molar Mass."""
+        return MOL.dfatoms['mass'].sum()
+
+    @property
+    def formule(self):
+        """Return the molecular formule."""
+        formule = ""
+        atoms = MOL.atoms_count
+        for at in sorted(atoms):
+            if atoms[at] > 0:
+                if atoms[at] == 1:
+                    formule += at
+
+                else:
+                    formule += at
+                    formule += str(atoms[at])
+
+        return formule
+
+    @property
+    def totq(self):
+        """Compute the total charge."""
+        return self.dftypes["charge"].sum()
+
+
+class ATOM(MOL):
+    """
+    Subclass to represent an atom of the parent class MOL.
+    """
+    def __init__(self, at, n):
+        """Initialize with the atomic symbol and the index in the molecule."""
+        self.atsb = at
+        self.n = n
+
+    @property
+    def atoms_connect(self):
+        """Returns a string with the symbols of the connected atoms and their
+        hybridization."""
+        hyb_atoms_connect = ""
+
+        if self.atsb == "H":
+            # HYDROGEN
+            at_i = list(MOL.connect.neighbors(self.n))[0]
+            sb = MOL.connect.nodes[at_i]["atsb"]
+            nbonds = len(list(MOL.connect.neighbors(at_i)))
+
+            if sb == 'C' and nbonds == 3:
+                """ H, aromatic hydrogen """
+                hyb_atoms_connect += "Csp2"
+
+            elif sb == 'C' and nbonds == 4:
+                """ H, aliphatic hydrogen """
+                hyb_atoms_connect += "Csp3"
+
+        elif self.atsb == "C":
+            # CARBON
+            # atoms bonded to carbon
+            for at_i in MOL.connect.neighbors(self.n):
+                sb = MOL.connect.nodes[at_i]["atsb"]
+                nbonds = len(list(MOL.connect.neighbors(at_i)))
+
+                if sb == "C" and nbonds == 3:
+                    hyb_atoms_connect += "Csp2"
+
+                elif sb == "C" and nbonds == 4:
+                    hyb_atoms_connect += "Csp3"
+
+                elif sb == "H":
+                    hyb_atoms_connect = "H" + hyb_atoms_connect
+        else:
+            raise MoleculeDefintionError(_errorMessages[2].format(sb))
+
+        return hyb_atoms_connect
+
+    @property
+    def hyb(self):
+        atms_hyb = {
+            "C": {4: "sp3", 3: "sp2"}
+        }
+        try:
+            return atms_hyb[self.sb][len(self.connect[self.n])]
+        except KeyError:
+            raise MoleculeDefintionError(_errorMessages[3].format(
+                self.n, self.atsb))
+
+
+def _load_xyz(file):
+    """Read a file xyz."""
+    coord = pd.read_csv(
+        file,
+        sep=r'\s+',
+        skiprows=2,
+        header=None,
+        names=['atsb', 'x', 'y', 'z'],
+        dtype={'x': np.float64, 'y': np.float64, 'z': np.float64}
+    )
+
+    coord["mass"] = coord["atsb"].apply(lambda at: Elements[at]["mass"])
+    coord["num"] = coord["atsb"].apply(lambda at: Elements[at]["num"])
+    # This file has no partial charges .
+    coord["charge"] = 0.0
+    return coord
