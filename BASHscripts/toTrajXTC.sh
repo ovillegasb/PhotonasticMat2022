fff8eddd42dd9b2a1ae6174bec0f1f2bbfb45548
Orlando Villegas <ragnar0317@gmail.com>
2022-05-10 10:25:41 +0200

    The script can read arguments

--- BASHscripts/toTrajXTC.sh
+++ BASHscripts/toTrajXTC.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+conf=$1
+NMOL=$2
+RES=$3
+NAT=$4
+
 for file in *.xyz
 do
 	echo $file
-	../xyz2gro $file 343 MET 5
+	xyz2gro $file $NMOL $RES $NAT
 done
 
 cat *.gro > traj.gro
 
 gmx trjconv -f traj.gro -o traj.xtc
 
-mv PasDeCalcul_Iteration_00100000.gro confout.gro
+mv "${conf%.*}.gro" confout.gro
+
+# rm *.xyz
