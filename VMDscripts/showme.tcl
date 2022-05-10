899520c903bf838c3be7277e52a7c3957a760a3a
Orlando Villegas <ragnar0317@gmail.com>
2022-05-10 10:24:37 +0200

    Files to configure the initial representation in VMD

---
+++ VMDscripts/showme.tcl
@@ -0,0 +1,15 @@
+
+# Show BOX
+pbc box -color black
+
+# Style
+mol modstyle 0 0 VDW 1.000000 12.000000
+mol modmaterial 0 0 AOEdgy
+
+# Background
+color Display Background white
+
+# Animation
+animate speed 0.78
+animate goto 0
+
