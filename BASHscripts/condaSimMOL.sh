d3b8a36c1552d370adf053ae6678ea8ae183814b
ovillegasb <ragnar0317@gmail.com>
2022-05-16 04:40:58 +0200

    adding new modules to the SimMOL configuration

--- BASHscripts/condaSimMOL.sh
+++ BASHscripts/condaSimMOL.sh
@@ -3,6 +3,13 @@
 conda create --name SimMOL scipy jupyterlab numpy matplotlib seaborn pandas
 conda activate SimMOL
 conda install --channel conda-forge pymatgen
+conda install nglview -c conda-forge
 
 # Others
 sudo dnf install pandoc
+conda install -c conda-forge nodejs
+conda install -c conda-forge/label/gcc7 nodejs
+conda install -c conda-forge/label/cf201901 nodejs
+conda install -c conda-forge/label/cf202003 nodejs
+conda install -n base -c conda-forge jupyterlab_widgets
+conda install -n SimMOL -c conda-forge ipywidgets
