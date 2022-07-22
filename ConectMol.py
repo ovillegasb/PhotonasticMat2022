cfce5ed1abea22cb47b064db19b9b114a6f3ec2d
Orlando Villegas <ragnar0317@gmail.com>
2022-07-22 12:21:32 +0200

    function for your consecutive numbers.

--- ConectMol.py
+++ ConectMol.py
@@ -29,6 +29,19 @@ def options():
     return vars(parser.parse_args())
 
 
+def group_runs(li, tolerance=2):
+    """Group consecutive integers and tolerate gaps of 1."""
+    out = []
+    last = li[0]
+    for x in li:
+        if x-last > tolerance:
+            yield out
+            out = []
+        out.append(x)
+        last = x
+    yield out
+
+
 args = options()
 
 fatoms = args["fatoms"]
@@ -68,48 +81,23 @@ for mol in atoms_MOL:
     mol = list(sorted(mol))
     bulk[ipol] = dict()
     bulk[ipol]["Natoms"] = len(mol)
-    bulk[ipol]["index"] = " ".join(["%d to %d" % (i, i + 9) for i in mol[0:-1:size]])
+    # bulk[ipol]["index"] = " ".join(["%d to %d" % (i, i + 9) for i in mol[0:-1:size]])
+    # print(sorted(mol))
+    m = sorted(mol)
+    groups = group_runs(m)
+
+    # print(list(groups))
+
+    ndx = " ".join(["%d to %d" % (lm[0], lm[-1]) for lm in groups])
+    bulk[ipol]["index"] = ndx
+    # bulk[ipol]["index"] = " ".join([str(i) for i in mol])
     ipol += 1
     if len(mol) > atoms:
         atoms = len(mol)
         indexs = mol
 
-# print(indexs)
-# print(atoms)
-
-# for vmd
-select = " ".join(["%d to %d" % (i, i + 9) for i in indexs[0:-1:10]])
-print(select)
-
-#
 
 with open("polysystem.json", "w") as f:
     json.dump(bulk, f, indent=0, sort_keys=True)
 
-# print(bulk)
-
-"""
-init = 0
-end = 0
-
-ixat = 0
-
-while ixat < len(indexs):
-    at1 = indexs[ixat]
-    fxat = 0
-    for at2 in indexs[ixat+1:]:
-        if at2 - at1 == 1:
-            at1 = at2
-            fxat = at2
-            # print(at1)
-            # print(at2)
-            # print(fxat)
-
-        elif at2 - at1 > 1:
-            ixat = at2 + 1
-            ixat = fxat + 1
-            print(ixat, fxat)
-            break
-
-    # break
-"""
+print("\nFile polysystem.json saved.")
