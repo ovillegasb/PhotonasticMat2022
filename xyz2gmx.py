3d8499a8d3a6a8eb1774a1fd6e8cf84188ab3d79
ovillegasb <ragnar0317@gmail.com>
2022-09-14 08:52:00 +0200

    program for convert xyz to gro files

---
+++ xyz2gmx.py
@@ -0,0 +1,86 @@
+
+import json
+import glob
+import numpy as np
+from molcraft import structure
+from stamptools.analysis import read_fatomes
+
+def load_conectivity(file):
+    # Opening JSON file
+    with open(file) as json_file:
+        data = json.load(json_file)
+
+    return data
+
+def save_gro(name, description, coord, box):
+        """
+        Write coordinates to file .gro.
+
+        """
+        nat = len(coord)
+
+        GRO = open('%s.gro' % name, 'w', encoding='utf-8')
+        GRO.write('%s\n' % description)
+        GRO.write('%5d\n' % nat)
+        for i in coord.index:
+            GRO.write('{:>8}{:>7}{:5d}{:8.3f}{:8.3f}{:8.3f}\n'.format(
+                coord.loc[i, 'resid'] + coord.loc[i, 'resname'],
+                coord.loc[i, 'atsb'].upper(),
+                i + 1,
+                coord.loc[i, 'x'] * 0.1,
+                coord.loc[i, 'y'] * 0.1,
+                coord.loc[i, 'z'] * 0.1)
+            )
+        GRO.write('   {:.5f}   {:.5f}   {:.5f}\n'.format(
+            box[0] * 0.1,
+            box[1] * 0.1,
+            box[2] * 0.1))
+
+        GRO.close()
+        print("file %s.gro writed" % name)
+
+
+# Reading FAtome
+_, box, _ = read_fatomes("FAtomes.inp_6_prod")
+
+
+# Load conectivity
+connect = load_conectivity("polysystem.json")
+
+
+# Load time per frame
+t_frame = np.loadtxt("t_frame.dat")
+
+# Load XYZ trajectory
+XYZ = glob.glob("XYZ/PasDeCalcul_*")
+traj = list()
+
+for file in XYZ:
+    xyz = structure.load_xyz(file, warning=False)
+    traj.append(xyz)
+
+
+# Gen resid and resname list
+resid = list()
+resname = list()
+for rid in connect:
+    idx = connect[rid]["index"].split()
+    res = "POL"
+    if rid == "0":
+        res = "AZO"
+    for _ in idx:
+        resid.append(rid)
+        resname.append(res)
+
+
+# for coord in traj:
+
+for i, coord in enumerate(traj):
+    coord["resname"] = resname
+    coord["resid"] = resid
+    coord["x"] += box[0]/2
+    coord["y"] += box[1]/2
+    coord["z"] += box[2]/2
+    description = "MD PC - Polymer, t=%.3f" % t_frame[i]
+    print(description)
+    save_gro("GRO/frame_%005d" % i, description, coord, box)
