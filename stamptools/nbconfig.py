c2cbc8e5c5548ca791255522b2d8879ba21199ce
Orlando Villegas <ragnar0317@gmail.com>
2023-07-06 14:38:54 +0200

    Corrections reading spectrums

--- stamptools/nbconfig.py
+++ stamptools/nbconfig.py
@@ -42,6 +42,45 @@ Ha_to_kJmol = 2625.5
 # Hatree to J
 Ha_to_J = 4.359748199e-18
 
+# PROP figures pol
+POLPROP = {
+    "Rg": {
+        "color": "#2a90a6",
+        "binwidth": .1,
+        "xlim": [0.5, 3.0],
+        "label": "Radius of gyration (nm)",
+        "units": "nm"
+    },
+    "dmax": {
+        "color": "#5b557b",
+        "binwidth": .3,
+        "xlim": [1.0, 9.0],
+        "label": "Max. distance (nm)",
+        "units": "nm"
+    },
+    "k2": {
+        "color": "#fea6ad",
+        "binwidth": .05,
+        "xlim": [0, 1],
+        "label": "Shape anisotropy",
+        "units": ""
+    },
+    "rdf": {
+        "color": "#3293f0",
+        "xlim": [0.25, 2.5],
+        "label": "g(r) PC - POL",
+        "units": ""
+
+    },
+    "distance": {
+        "color": "#66c0c0",
+        "binwidth": .1,
+        "xlim": [0, 4.0],
+        "label": "distance",
+        "units": "nm"
+    }
+}
+
 
 """ Some Functions. """
 
@@ -250,7 +289,7 @@ def dnorm(values):
     return norm.pdf(values, values.mean(), values.std())
 
 
-def get_spectre_info(logs_files):
+def get_spectre_info(logs_files, dtype="cont"):
     """."""
     data = {}
     info = []
@@ -259,12 +298,33 @@ def get_spectre_info(logs_files):
         data[i] = read_UVVis(file)
         metadata = file.split("/")[4:]
         # print(i, metadata[0], metadata[1], metadata[-1].split(".")[-2].split("_")[-1])
-        info.append({
-            "index": i,
-            "isomer": metadata[0],
-            "replica": int(metadata[1].split("_")[-1]),
-            "frame": int(metadata[-1].split(".")[-2].split("_")[-1])
-        })
+        if dtype=="GMX":
+            info.append({
+                "index": i,
+                "isomer": metadata[0].split(".")[0].split("_")[1],
+                "replica": int(metadata[1].replace("md", "")),
+                "frame": int(metadata[-1].split(".")[-2].split("_")[-1])
+            })
+        else:
+            try:
+                info.append({
+                    "index": i,
+                    "isomer": metadata[0],
+                    "replica": int(metadata[1].split("_")[-1]),
+                    "frame": int(metadata[-1].split(".")[-2].split("_")[-1])
+                })
+            except ValueError:
+                metadata = [
+                    file.split("/")[2].split(".")[0].split("_")[1],
+                    0,
+                    file.split("/")[-1].split(".")[0].split("_")[-1]
+                ]
+                info.append({
+                    "index": i,
+                    "isomer": metadata[0],
+                    "replica": metadata[1],
+                    "frame": int(metadata[2])
+                })
         # print(file.split("/")[-1].split(".")[-2].split("_")[-1])
         
     print("Number of files analyzed:", len(data))
@@ -272,19 +332,64 @@ def get_spectre_info(logs_files):
     spectra = []
     w_s1 = []
     w_s2 = []
+    w_s3 = []
+    w_s4 = []
+    w_s5 = []
+    w_s6 = []
+
+    f_s1 = []
+    f_s2 = []
+    f_s3 = []
+    f_s4 = []
+    f_s5 = []
+    f_s6 = []
+
     for i in data:
         spectra.append(epsilon_tot(w_nm, data[i]))
         w_s1.append(data[i].loc[0, "wl"])
         w_s2.append(data[i].loc[1, "wl"])
+        w_s3.append(data[i].loc[2, "wl"])
+        w_s4.append(data[i].loc[3, "wl"])
+        w_s5.append(data[i].loc[4, "wl"])
+        w_s6.append(data[i].loc[5, "wl"])
+
+        f_s1.append(data[i].loc[0, "f"])
+        f_s2.append(data[i].loc[1, "f"])
+        f_s3.append(data[i].loc[2, "f"])
+        f_s4.append(data[i].loc[3, "f"])
+        f_s5.append(data[i].loc[4, "f"])
+        f_s6.append(data[i].loc[5, "f"])
         
     spectra = np.array(spectra).mean(axis=0)
     w_s1 = np.array(w_s1)
     w_s2 = np.array(w_s2)
+    w_s3 = np.array(w_s3)
+    w_s4 = np.array(w_s4)
+    w_s5 = np.array(w_s5)
+    w_s6 = np.array(w_s6)
+
+    f_s1 = np.array(f_s1)
+    f_s2 = np.array(f_s2)
+    f_s3 = np.array(f_s3)
+    f_s4 = np.array(f_s4)
+    f_s5 = np.array(f_s5)
+    f_s6 = np.array(f_s6)
 
     dfinfo = pd.DataFrame(info)
     dfinfo.set_index("index", inplace=True)
     dfinfo["s1"] = w_s1
     dfinfo["s2"] = w_s2
+    dfinfo["s3"] = w_s3
+    dfinfo["s4"] = w_s4
+    dfinfo["s5"] = w_s5
+    dfinfo["s6"] = w_s6
+
+    dfinfo["f1"] = f_s1
+    dfinfo["f2"] = f_s2
+    dfinfo["f3"] = f_s3
+    dfinfo["f4"] = f_s4
+    dfinfo["f5"] = f_s5
+    dfinfo["f6"] = f_s6
     
     return spectra, dfinfo
 
@@ -310,3 +415,4 @@ def showInfo():
         Binned distances (in nm).
 
     """)
+
