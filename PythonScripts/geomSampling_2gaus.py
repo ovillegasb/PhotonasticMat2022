af41b72f1f9da1179d070bd1afe4fb3e7d7bd072
Orlando Villegas <ragnar0317@gmail.com>
2023-12-08 18:05:01 +0100

    Sampling script for TDDFT enhanced to support different trajectories.

--- PythonScripts/geomSampling_2gaus.py
+++ PythonScripts/geomSampling_2gaus.py
@@ -3,6 +3,7 @@
 
 import argparse
 import numpy as np
+import pandas as pd
 import mdtraj as md
 from pymatgen.core import Molecule
 from pymatgen.io.gaussian import GaussianInput
@@ -46,7 +47,7 @@ def options():
         "-o", "--out",
         help="Output folder.",
         type=str,
-        default="SamplingUV-Vis"
+        default="SamplesTDDFT"
     )
 
     analysis = parser.add_argument_group(
@@ -77,7 +78,7 @@ def options():
         "-dt", "--dt",
         help="Frame generation step (default unit ps).",
         type=float,
-        default=2.5
+        default=1.0
     )
 
     analysis.add_argument(
@@ -98,7 +99,7 @@ def options():
         "-isomer", "--isomer",
         help="Isomer type.",
         type=str,
-        default=None
+        default=""
     )
 
     analysis.add_argument(
@@ -118,30 +119,83 @@ def clean_folder(folder):
         os.remove(f)
 
 
+def read_xyzTraj(trajXYZ):
+    """Return the information of an XYZ trajectory file."""
+    xyz_list = []
+    with open(trajXYZ, "r") as XYZ:
+        nline_tot = 0
+        nsaved = 0
+        for line in XYZ:
+            line = line.replace("\n", "").split()
+            if len(line) == 1:
+                nline_tot = int(line[0])
+                nsaved = 0
+                frame_values = []
+                _ = XYZ.readline()
+                continue
+
+            elif len(line) == 4:
+                frame_values.append(np.array(line))
+                nsaved += 1
+
+            if nsaved == nline_tot:
+                frame = np.array(frame_values)
+                atsb = frame[:, 0].astype(str)
+                x = frame[:, 1].astype(np.float64)
+                y = frame[:, 2].astype(np.float64)
+                z = frame[:, 3].astype(np.float64)
+                frame = pd.DataFrame({
+                    "atsb": atsb,
+                    "x": x,
+                    "y": y,
+                    "z": z
+                })
+                xyz_list.append(frame)
+
+    return xyz_list
+
+
 def main():
     """Run program."""
     args = options()
 
     out = args["out"]
     top = args["topol"]
-    assert top is not None, "You have to define a topology file."
+    assert top is not None or args["traj"].endswith("xyz"), "You have to define a topology file."
     trj = args["traj"]
     assert trj is not None, "You have to define a trajectory file."
     isomer = args["isomer"]
     assert isomer is not None, "You have to define a isomer type."
 
-    b = int(args["b"])
-    e = int(args["e"])
-    out += "_{}_{}".format(str(b), str(e))
+    resid = args["resid"]
+    out += "_r{}".format(resid)
+
+    b = float(args["b"])
+    if b != 0.0:
+        out += "_{:.1f}_to".format(b)
+
+    try:
+        e = float(args["e"])
+    except TypeError:
+        out += ""
+        e = -1
+    else:
+        out += "_{:.1f}".format(e)
+
+    solvent = args["solvent"]
+    if len(solvent) > 0:
+        out += "_{}".format(solvent)
+
+    if len(isomer) > 0:
+        out += "_{}".format(isomer)
+
+    samples_number = args["NSamples"]
+    out += "_N{}".format(samples_number)
 
     if not os.path.exists(out):
         os.mkdir(out)
     else:
         clean_folder(out)
-
-    resid = args["resid"]
-    solvent = args["solvent"]
-    samples_number = args["NSamples"]
 
     print("File top:", top)
     print("File trj:", trj)
@@ -153,13 +207,15 @@ def main():
     print("Time >=", b, "ps", end=" - ")
     b -= init_t
     begin = int(b / dt)
-    print("index:", begin)
+    print("frame b:", begin)
 
-    print("Time <", e, "ps", end=" - ")
-    e -= init_t
-    end = int(e / dt)
-    print("index:", end)
-
+    if e != -1:
+        print("Time <", e, "ps", end=" - ")
+        e -= init_t
+        end = int(e / dt)
+    else:
+        print("frame e: Last")
+        end = None
     print("Resid:", resid)
 
     #
@@ -173,7 +229,10 @@ def main():
             print("ERROR solvent not recognized in the list")
             exit()
 
-    if top.endswith("gro"):
+    frames_sample = []
+
+    if top is not None and not trj.endswith("xyz"):
+        assert top.endswith("gro"), "You have to define a topology gro file."
         # Reads the system trajectory
         t = md.load(trj, top=top)
         print("Total frames in trajectory:", t.n_frames)
@@ -191,27 +250,59 @@ def main():
         # The indices of the atoms of interest are selected.
         iat_res = top.select("resid %s" % resid)
 
-        # for frame to gaussian input
-        for i in frames_sample:
+    elif trj.endswith("xyz"):
+        # Read the system topolgy
+        xyzTraj = read_xyzTraj(trj)
+        print("Total frames in trajectory:", len(xyzTraj))
+
+        traj = xyzTraj[begin:end]
+        print("Frames selected:", len(traj))
+        frames_sample = np.random.choice(
+            range(len(traj)), samples_number, replace=False
+        )
+
+        atoms = traj[0]["atsb"].values
+        xyz = []
+        for frame in traj:
+            xyz.append(frame.loc[:, ["x", "y", "z"]].values)
+        xyz = np.array(xyz)
+
+    # for frame to gaussian input
+    for i in frames_sample:
+
+        if not trj.endswith("xyz"):
             atoms = table.loc[iat_res, "element"].values
             coord = traj.xyz[:, iat_res][i] * 10.0  # to angstroms
+        else:
+            coord = xyz[i]
     
-            mol = Molecule(atoms, coord)
+        mol = Molecule(atoms, coord)
+        try:
+            iso = isomer[0].upper() + "_"
+        except IndexError:
+            iso = ""
+
+        if len(solvent) == 0:
+            sol = ""
+        else:
+            sol = solvent + "_"
     
-            # save file gaussian
-            GaussianInput(
-                mol,
-                charge=0,
-                spin_multiplicity=1,
-                title="Azobenzene %s Sampling in %s - sample %d" % (isomer, solvent, i),
-                functional="Cam-B3LYP",
-                basis_set="6-311+g(d,p)",
-                route_parameters=route_parameters,
-                link0_parameters={
-                    "%mem": "8GB",
-                    "%nprocshared": "12"
-                }
-            ).write_file("%s/azo%s_%s_%005d.com" % (out, isomer[0].upper(), solvent, i))
+        # save file gaussian
+        GaussianInput(
+            mol,
+            charge=0,
+            spin_multiplicity=1,
+            title="Azobenzene %s Sampling in %s - sample %d" % (isomer, solvent, i),
+            functional="Cam-B3LYP",
+            basis_set="6-311+g(d,p)",
+            route_parameters=route_parameters,
+            link0_parameters={
+                "%mem": "8GB",
+                "%nprocshared": "12"
+            }
+        ).write_file("%s/azo_%s%s%005d.com" % (out, iso, sol, i))
+
+    print("Output folder: \033[1;36m{:>15}\033[m".format(out))
 
 
 if __name__ == '__main__':
